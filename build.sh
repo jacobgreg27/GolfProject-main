b2ee66b75dac205063ae398e4560b83d20fb05a6
jacobgreg27 <jgdintu@gmail.com>
2023-05-08 15:15:32 +0400

    change

--- build.sh
+++ build.sh
@@ -8,5 +8,7 @@ cd django_sms
 pip install -r requirements.txt
 
 if [[ $CREATE_SUPERUSER ]]
-then 
-python manage.py createsuperuser --no-input fi
+then
+python manage.py createsuperuser --no-input
+fi
+``` 
