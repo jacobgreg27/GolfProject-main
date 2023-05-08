7378037e5013cc58c7ceb55c19adc5de8fa92f54
jacobgreg27 <jgdintu@gmail.com>
2023-05-08 14:49:08 +0400

    SUPERUSER

--- django_sms/build.sh
+++ django_sms/build.sh
@@ -3,3 +3,7 @@
 pip install --upgrade pippip install --force-reinstall -U setuptools
 
 pip install -r requirements.txt
+
+if [[$CREATE_SUPERUSER == "true"]]; then
+    python manage.py createsuperuser --noinput
+fi
--- django_sms/django_sms/settings.py
+++ django_sms/django_sms/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-j7xv%)66l8u0^8sxnj7()z$zll8&a2*_h2r84yeo9f2fidx8^7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
