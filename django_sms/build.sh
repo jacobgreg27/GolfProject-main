eb47d7115f53fbb272b6e3bf6405a5d913961471
jacobgreg27 <jgdintu@gmail.com>
2023-05-08 15:20:44 +0400

    changes

--- django_sms/build.sh
+++ django_sms/build.sh
@@ -3,8 +3,13 @@
 pip install --upgrade pippip install --force-reinstall -U setuptools
 
 pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]]
 then 
 python manage.py createsuperuser --no-input
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(email='$SUPERUSER_EMAIL').update(password='$SUPERUSER_PASSWORD')"
 fi
+password=$password
+python manage.py runserver
