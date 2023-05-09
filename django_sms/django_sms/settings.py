f159a456c2192899134080edf9689e58c65f8bae
jacobgreg27 <jgdintu@gmail.com>
2023-05-09 11:54:32 +0400

    settings

--- django_sms/django_sms/settings.py
+++ django_sms/django_sms/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-j7xv%)66l8u0^8sxnj7()z$zll8&a2*_h2r84yeo9f2fidx8^7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
