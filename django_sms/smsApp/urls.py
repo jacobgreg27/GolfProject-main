72f2bf22160d03379f267d92f7032fceb310ce75
jacobgreg27 <jgdintu@gmail.com>
2023-05-12 12:34:00 +0400

    urls changes

--- django_sms/smsApp/urls.py
+++ django_sms/smsApp/urls.py
@@ -55,6 +55,4 @@ urlpatterns = [
     path("scanner_view/", views.scanner_view, name="scanner-view"),
 ]
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
